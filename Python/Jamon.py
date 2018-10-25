353cf5dab836c379904c970c756ea3b690c69d6f
IanRull <44442069+IanRull@users.noreply.github.com>
2018-10-25 17:23:58 +0100

    Create Jamon.py

---
+++ Python/Jamon.py
@@ -0,0 +1,13 @@
+jamon = input("Has comprado un jamon?")
+
+abduccion = input("Te han abdicido?")
+
+dia = input("Es jueves?")
+
+estudiar = input("Has estudiado mucho?")
+
+if (jamon =="si")or((abduccion == "si")and(dia == "si"))or(estudiar == "si"): print("Aprovar m03")
+
+else:
+
+print("No apruebas m03")
