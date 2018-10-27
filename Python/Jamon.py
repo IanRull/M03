f8f3e653119b0043bf2d213d37dbdc547c30822e
IanRull <44442069+IanRull@users.noreply.github.com>
2018-10-27 21:00:54 +0100

    Update Jamon.py

--- Python/Jamon.py
+++ Python/Jamon.py
@@ -1,12 +1,13 @@
 jamon = input("Has comprado un jamon?")
 
-abduccion = input("Te han abdicido?")
+abduccion = input("Te han abducido?")
 
 dia = input("Es jueves?")
 
 estudiar = input("Has estudiado mucho?")
 
-if (jamon =="si")or((abduccion == "si")and(dia == "si"))or(estudiar == "si"): print("Aprovar m03")
+if (jamon =="si")or((abduccion == "si")and(dia == "si"))or(estudiar == "si"): 
+  print("Aprovar m03")
 
 else:
 
