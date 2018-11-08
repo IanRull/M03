926f98aaa05bb0b9a8c29637151d0d07b3d50422
IanRull <44442069+IanRull@users.noreply.github.com>
2018-11-08 12:17:41 +0100

    Update and rename PositivoNegatico.py to PositivoNegatico_1.1.py

--- Extras/PositivoNegatico.py
+++
@@ -1,6 +0,0 @@
-#coding:utf-8
-numero = int(input("Ponga un numero:"))
-if(numero >= 0):
-    print("Es positivo")
-else:
-    print("Es negativo")
---
+++ Extras/PositivoNegatico_1.1.py
@@ -0,0 +1,43 @@
+#V1.1
+#coding:utf-8
+numero = int(input("Ponga un numero:"))
+if(numero >= 0):
+    print("Es positivo")
+else:
+    print("Es negativo")
+#V1.2
+###########################################################################################
+#                                     JOC DE PROVES                                       #                                                #
+#              ENTRADA                                                SORTIDA             #                                                                  #
+#                 3                                                  'POSITIU'            #
+#                -2                                                  'NEGATIU'            #       
+#                 0                                                  'POSITIU'            #       
+#                                                                                         #
+#                                                                                         #
+###########################################################################################
+
+#coding:utf-8
+numero = int(input("Ponga un numero:"))
+if(numero ==0):
+    print("Es positivo")
+elif(numero > 0):
+    print("Es negativo")
+else:
+    print("Es positivo")
+         
+        
+#V1.3
+###########################################################################################
+#                                     JOC DE PROVES                                       #                                                #
+#              ENTRADA                                                SORTIDA             #                                                                  #
+#                 1  8      7                                          1      8         #
+#                -2  -3     4                                           -3    -2         #       
+#                 7   0    0                                            0      7         #       
+#                 0   0   0                                            0      0         #
+#                                                                                         #
+###########################################################################################
+#coding:utf-8
+numeros=list(eval(input("Posa tres numeros separats per comes: ")))
+numeros.sort()
+print(numeros)
+
