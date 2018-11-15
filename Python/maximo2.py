70abb6716c727674be2bd2e4477e88435d23a42c
IanRull <44442069+IanRull@users.noreply.github.com>
2018-11-15 10:31:42 +0100

    Update maximo2.py

--- Python/maximo2.py
+++ Python/maximo2.py
@@ -20,3 +20,56 @@ else:
                 else:
                     if(numero1 > numero2):
                         print("el" ,numero1,"es el mas grande")
+
+             
+                        
+                      OTRA OPCION MAS CORTA
+#coding:utf-8
+# Comparador de numeros##
+print("Buenas, aqui puedes comparar entre 2 numeros cual es el mas grande :D")
+numero1 = int(input("Ponga un numero"))
+numero2 = int(input("Ponga un numero :"))
+if(numero1 == 0 and numero2 == 0):
+    print("No pueden ser 0 los numeros")
+else:
+    if(numero1 < numero2)or(numero1 == numero2):
+        print("el" ,numero2,"es el mas grande")
+    else:
+        if(numero1 > numero2)or(numero2 == numero1):
+            print("el" ,numero1,"es el mas grande")
+            
+                     OTRA OPCION AUN MAS CORTA
+                
+                #coding:utf-8
+# Comparador de numeros##
+print("Buenas, aqui puedes comparar entre 2 numeros cual es el mas grande :D")
+numero1 = int(input("Ponga un numero"))
+numero2 = int(input("Ponga un numero :"))
+if(numero1 < numero2):
+    print("el" ,numero2,"es el mas grande")
+else:
+    print("El",numero1,"es el mas grande")
+    
+    
+
+    AHORA DE 3 
+    
+    
+#coding:utf-8
+# Comparador de numeros##
+print("Buenas, aqui puedes comparar entre 2 numeros cual es el mas grande :D")
+numero1 = float(input("Ponga un numero"))
+numero2 = float(input("Ponga un numero :"))
+numero3 = float(input("Ponga otro numero : "))
+if(numero1 > numero2 > numero3):
+    print(numero1)
+else:
+    if(numero2 >= numero1 >= numero3):
+        print(numero2)
+    else:
+        print(numero3)
+        
+            
+            
+      
+            
