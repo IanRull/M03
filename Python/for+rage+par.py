0d3d72fd2ee2ae62d7d80fd6786fdd254b0c659c
IanRull <44442069+IanRull@users.noreply.github.com>
2018-11-20 05:34:14 +0100

    Create for+rage+par.py

---
+++ Python/for+rage+par.py
@@ -0,0 +1,10 @@
+#python 3.6##
+#coding:utf-8##
+for i in range(0, 30):
+    if ( range == 0):
+        print('PAR')
+    else:
+        if i % 2 == 0:
+            print('PAR')
+        else:
+            print('NADA')
