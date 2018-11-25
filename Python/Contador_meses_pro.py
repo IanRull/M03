53ec65e7552f957597bdf2fcdc0f5754e660e232
IanRull <44442069+IanRull@users.noreply.github.com>
2018-11-25 12:29:22 +0100

    Create Contador_meses_pro.py

---
+++ Python/Contador_meses_pro.py
@@ -0,0 +1,52 @@
+#python 3.6#i#
+#coding:utf-8#a#
+#Contador meses#n#
+dia = 'nothing'
+for x in range(0,30):
+    print(x +2)
+if(x > 0)and((x +1 )<= 30):
+    print('ENERO')
+for x in range(30 ,59):
+    print(x + +1)
+if(x > 30)and((x +1 )<= 59):
+    print('FEBRERO')
+for x in range(59 ,90):
+    print(x + 1)
+if(x > 59)and((x +1 )<= 90):
+    print('MARZO')
+for x in range(90 ,120):
+    print(x + 1)
+if(x > 99)and((x +1 )<= 120):
+    print('ABRIL')
+for x in range(120 ,151):
+    print(x + 1)
+if(x > 120)and(x <= 151):
+    print('MAYO')
+for x in range(151 ,181):
+    print(x + 1)
+if(x > 151)and(x <= 181):
+    print('JUNIO')
+for x in range(181 ,212):
+    print(x + 1)
+if(x > 181)and(x <= 212):
+    print('JULIO')
+for x in range(212 ,243):
+    print(x + 1)
+if(x > 212)and(x <= 243):
+    print('AGOSTO')
+for x in range(243 ,273):
+    print(x + 1)
+if(x > 243)and(x <= 273):
+    print('SEPTIEMBRE')
+for x in range(273 ,304):
+    print(x + 1)
+if(x > 273)and(x <= 304):
+    print('OCTUBRE')
+for x in range(304 ,334):
+    print(x + 1)
+if(x > 304)and(x <= 334):
+    print('NOBIEMBRE')
+for x in range(334 ,365):
+    print(x + 1)
+if(x > 334)and(x <= 365):
+    print('DICIEMBRE')
