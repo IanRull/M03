670194c5a01c6caa167cb9941dd67887491ea975
IanRull <44442069+IanRull@users.noreply.github.com>
2018-11-27 13:03:04 +0100

    Update division-exacta.py

--- Python/division-exacta.py
+++ Python/division-exacta.py
@@ -4,7 +4,7 @@
 dividen = int(input('Escriba el dividendo: '))
 divis = int(input('Escriba el divisor: '))
 if (dividen == 0)or(divis == 0):
-        print('Cabia el numero, el cero no vale ')
+        print('Cabia el numero, el cero no vale,podrias crear un bucle que destruiria el universo. ')
 else:
     modulo = dividen%divis
     print('La division es :',dividen/divis)
